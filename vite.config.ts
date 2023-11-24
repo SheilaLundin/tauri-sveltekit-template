6e07e60cb68227d3b2a9766b9c2a9bf77c752d78
Chaos <sheilalundin@163.com>
2023-11-24 12:46:48 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+	//
+	// 1. prevent vite from obscuring rust errors
+	clearScreen: false,
+	// 2. tauri expects a fixed port, fail if that port is not available
+	server: {
+		port: 7867,
+		strictPort: true,
+	},
+	// 3. to make use of `TAURI_DEBUG` and other env variables
+	// https://tauri.app/v1/api/config#buildconfig.beforedevcommand
+	envPrefix: ["VITE_", "TAURI_"],
+});
